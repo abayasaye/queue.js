59abab4deeeb46ee7ab317ab43c7b2d060bbc964
abayasaye <abay400@gmail.com>
2022-08-30 20:14:12 +0300

    n

--- main.js
+++ main.js
@@ -95,14 +95,31 @@ class Node {
 class LinkedList {
     constructor(){
         this.head = null;
-        this.size = 0;
+        this.size++;
     }
     insertFirst(data){
         this.head = new Node(data , this.head)
     }
-    insertLast(){
-        
+
+    insertLast(data){
+        let node = new Node(data);
+        let current;
+
+        if(!this.head){
+            this.head =node;
+        }else{
+            current = this.head;
+
+            while (current.next) {
+                current = current.next;
+
+            }
+            current.next = node;
+        }
+        this.size++
     }
+
+
     PrintListData(){
         let current = this.head;
         while (current) {
@@ -110,9 +127,39 @@ class LinkedList {
             current = current.next
         }
     }
+    
+
+    insertIndex(data , index){
+        if(index > 0 && index > this.size){
+        return;
+        }
+        if(index === 0){
+            this.head = new Node(data , this.head)
+            return;
+        }
+        const node = new Node(data);
+        let current , previous;
+        
+        current = this.head;
+        let count = 0;
+        
+        while(count < index){
+            previous = current;
+            count++;
+            current = current.next;
+        }
+
+        node.next = current;
+        previous.next = node;
+
+        this.size++;
+    }
 }
 const ll = new LinkedList();
 ll.insertFirst(100);
 ll.insertFirst(200);
 ll.insertFirst(300);
+ll.insertLast(400);
+ll.insertIndex(500 , 2)
+
 ll.PrintListData();
