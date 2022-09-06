9f656cef1e45b90425b53ae859f8248264f82b08
abayasaye <abay400@gmail.com>
2022-09-06 16:01:42 +0300

    n

---
+++ Tree.js
@@ -0,0 +1,31 @@
+function Node(value){
+    this.value = value;
+    this.children = [];
+}
+
+function Tree(rootNode){
+    this.root = rootNode;
+
+    const addNode = (childNodes, parentNode) => {
+        parentNode.children.push(...childNodes);
+    }
+
+
+
+    return{
+        addNode,
+        root
+    };
+}
+
+const root = new Node("A")
+const tree = new Tree(root)
+const bNode = new Node("B")
+const cNode = new Node("C")
+const dNode = new Node("D")
+const eNode = new Node("E")
+
+
+tree.addNode([bNode,cNode],root);
+tree.addNode([dNode,eNode],cNode);
+console.log(tree.root)
